84d73327abfe6a388f57f44fa6c1de76cc3d1733
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-03-27 15:03:22 -0300

    Update function to also return the total memory of the system

--- src/modules/get_memory_info.py
+++ src/modules/get_memory_info.py
@@ -17,6 +17,8 @@ def get_memory_info():
 
     memory_information_list = []
 
+    memory_total = 0
+
     for mem in memory:
         slot_information = {}
         for prop in properties_map:
@@ -47,6 +49,7 @@ def get_memory_info():
                     else:
                         slot_information[properties_map[prop]] = value
                 elif prop == "Capacity":
+                    memory_total += int(value)
                     megabytes = u.convert_bytes_to_megabytes(value)
                     slot_information[properties_map[prop]] = f"{megabytes:.0f} MB"
                 elif prop == "Speed":
@@ -60,7 +63,8 @@ def get_memory_info():
     memory_array = u.get_single_wmi_instance(u.W.Win32_PhysicalMemoryArray)
     total_slots = getattr(memory_array, "MemoryDevices", None)
     slots_used = {
-        "Slots used": f"{len(memory)} of {total_slots}"
+        "Slots used": f"{len(memory)} of {total_slots}",
+        "Size": f"{u.convert_bytes_to_gigabytes(memory_total):.0f} GB"
     }
     memory_information_list.append(slots_used)
 
