b4161481db594bc213c692fcde102154f568c2aa
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-03-27 09:10:16 -0300

    Display windows root partition (c:) usage info

--- src/modules/get_os_info.py
+++ src/modules/get_os_info.py
@@ -1,4 +1,5 @@
 import utils as u
+import psutil
 
 ###############################################################################
 # [[ Get Operating System information ]]
@@ -13,14 +14,29 @@ def get_os_info():
         "InstallDate": "Install Date"
     }
 
-    operating_system_info = {}
+    operating_system_information_list = []
 
+    os_info = {}
     for prop in properties_map:
         value = getattr(operating_system, prop, None)
         if value is not None:
             if prop == "InstallDate":
-                operating_system_info[properties_map[prop]] = u.convert_date_format(value)
+                os_info[properties_map[prop]] = u.convert_date_format(value)
             else:
-                operating_system_info[properties_map[prop]] = value
+                os_info[properties_map[prop]] = value
+    operating_system_information_list.append(os_info)
 
-    return [operating_system_info]
+    # Get disk usage from C: partition
+    total, used, free, percent = psutil.disk_usage("C:")
+    os_partition_usage = {
+        "Total":   f"{u.convert_bytes_to_gigabytes(total):.0f} GB",
+        "Used":    f"{u.convert_bytes_to_gigabytes(used):.0f} GB",
+        "Free":    f"{u.convert_bytes_to_gigabytes(free):.0f} GB",
+        "Percent": f"{percent}%",
+    }
+    os_partition_info = {
+        "Local Disk (C:)": os_partition_usage
+    }
+    operating_system_information_list.append(os_partition_info)
+
+    return operating_system_information_list
