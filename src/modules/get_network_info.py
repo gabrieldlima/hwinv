7221b533229039417a617435c5cce5982cd57cb0
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-03-27 15:05:32 -0300

    Only get valid (physical) network adapters info

--- src/modules/get_network_info.py
+++ src/modules/get_network_info.py
@@ -4,7 +4,7 @@ import utils as u
 # [[ Get Network information ]]
 ###############################################################################
 def get_network_info():
-    networks = u.get_network_wmi_instance(u.W.Win32_NetworkAdapter)
+    networks = u.get_all_wmi_instance(u.W.Win32_NetworkAdapter)
 
     properties_map = {
         "Description": "Description", 
@@ -16,10 +16,12 @@ def get_network_info():
 
     for network in networks:
         network_information = {}
-        for prop in properties_map:
-            value = getattr(network, prop, None)
-            if value is not None:
-                network_information[properties_map[prop]] = value
-        network_information_list.append(network_information)
+        description = getattr(network, "Description", None)
+        if not u.check_string(description):
+            for prop in properties_map:
+                value = getattr(network, prop, None)
+                if value is not None:
+                    network_information[properties_map[prop]] = value
+            network_information_list.append(network_information)
 
     return network_information_list
